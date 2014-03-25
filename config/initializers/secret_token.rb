0fe9cbc142a31d0bdea4998b0588672e2128822e
Adam89 <Adamm1@aston.ac.uk>
2014-03-25 14:52:02 +0100

    created initial rails app and specified required gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialGroupBuying::Application.config.secret_key_base = 'b3017736ac209e699d611640b8e6d0246c51a21ce03b37ffee8448e7d76d56d9abf54e4df196497b7cebb8d1eac1c977a2b4153b42c9f2532f9b4f4a83a87823'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialGroupBuying::Application.config.session_store :cookie_store, key: '_social-group-buying_session'
