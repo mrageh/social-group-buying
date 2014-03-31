cccebb37dee4a44082e66bad49db019458a1f2cb
Adam89 <adam@apprentice.io>
2014-03-31 18:26:30 +0200

    moved welcome message to I18n

--- spec/features/user_signs_in_spec.rb
+++ spec/features/user_signs_in_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'User signs in' do
   scenario 'with valid credentials' do
-    create_user(
+    user = create_user(
       name: 'Adam',
       email: 'test@example.com',
       password: 'secret',
@@ -15,7 +15,7 @@ feature 'User signs in' do
     sign_in_with('test@example.com', 'secret')
 
     expect(page).to have_text I18n.t('flashes.successfully_signed_in')
-    expect(page).to have_text 'Signed in as: Adam'
+    expect(page).to have_text I18n.t('signed_in_as', name: user.name)
   end
 
   scenario 'with invalid credentials' do
--- spec/features/user_signs_out_spec.rb
+++ spec/features/user_signs_out_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 
 feature 'User signs out' do
   scenario 'after signing in' do
-    create_user(
+    user = create_user(
       name: 'Adam',
       email: 'test@example.com',
       password: 'secret',
@@ -13,6 +13,6 @@ feature 'User signs out' do
     click_on I18n.t('button.sign_out')
 
     expect(page).to have_text I18n.t('flashes.successfully_signed_out')
-    expect(page).not_to have_text 'Signed in as: Adam'
+    expect(page).not_to have_text I18n.t('signed_in_as', name: user.name)
   end
 end
